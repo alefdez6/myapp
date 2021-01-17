3e602bba929be81a2bd6c580f31b8a03ba302303
Alejandro Fdez <alefdez95@gmail.com>
2021-01-17 10:20:03 +0000

    Prueba

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
+from flask import jsonify
 from flask import Flask
+from . import catalog
+
 app = Flask(__name__)
+
+@app.route('/product'
+def list_all_products():
+    '''This view retrieves all the products in the catalog'''
+    response = get_products()
+    return jsonify(response)
 
 @app.route('/hello')
 def hello_world():
-    return ("Hola Mundo, soy Python!")
+    message = "Hola mundo, soy Python! Ahora con CloudBuild y hablando JSON"
+    response = {
+            "message": message,
+            "length": len(message)
+    }
+    return jsonify(response)
 
 @app.route('/bye')
 def bye_world():
