613615a99386004efe0a0a0cd830eca54df43c2e
Alejandro Fdez <alefdez95@gmail.com>
2021-01-17 09:44:26 +0000

    Iitial commit

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+    return ("Hola Mundo, soy Python!")
+
+@app.route('/bye')
+def bye_world():
+    return ("Adios perro")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
+
